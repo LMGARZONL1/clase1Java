81b180e8fa01119afe33bf119beb32d3f816b458
Oscar Andres De Los Rios Muñoz <lanoican_10@hotmail.com>
2023-10-27 11:06:31 -0500

    x

---
+++ src/main/java/org/Exercise/Main.java
@@ -0,0 +1,30 @@
+package org.Exercise;
+
+public class Main {
+    public static void main(String[] args){
+        int x,y,z, sum,res,mult=0;
+        float mod;
+
+        x=4;
+        y=12;
+        z=4;
+
+        res=x+y+z;
+        System.out.println(res);
+        res=x-y-z;
+        System.out.println(res);
+        mult=x+y+z;
+        System.out.println(mult);
+
+        mod= y%x;
+        System.out.println(mod);
+
+    }
+}
+
+
+    //String name="";
+    //int numero=0;
+   // float ft=1.1f;
+   // char ch='a';
+   // boolean tr= true;
---
+++ src/main/java/org/example/Main2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Locale;
+
+public class Main2 {
+    public static void main(String[] args) {
+        String helloWord="Este es mi primer hola mundo";
+        System.out.println(helloWord.length());
+        System.out.println(helloWord.toLowerCase());
+        System.out.println(helloWord.toUpperCase());
+        int entero=58;
+        //System.out.println(helloWord.indexOf("es"));
+        String helloWord2="y voy muy bien";
+        String espacio=" ";
+        System.out.println(helloWord+espacio.concat(Integer.toString(entero)));
+    }
+}
